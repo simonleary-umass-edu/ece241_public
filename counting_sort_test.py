a459bb7e0f3d07cd2a1d082a4004a9e0c41c6765
simonLeary42 <71396965+simonLeary42@users.noreply.github.com>
2021-09-14 15:45:52 -0400

    re-copied from other repo to preserve original formatting

--- counting_sort_test.py
+++ counting_sort_test.py
@@ -10,13 +10,19 @@ def counting_sort():
     for i in range(len(alist)):
         c[alist[i]] = c[alist[i]] + 1
 
-    for i in range(1, largest +1):
+    # Find the last index for each element
+    # c[0] = c[0] - 1  # to decrement each element for zero-based indexing
+    for i in range(1, largest + 1):
         c[i] = c[i] + c[i - 1]
 
     result = [None] * len(alist)
 
-    for j in range(len(alist)-1, -1, -1):
-        result[c[alist[j]]-1] = alist[j]
+    # Though it is not required here,
+    # it becomes necessary to reverse the list
+    # when this function needs to be a stable sort
+    # for x in reversed(alist):
+    for j in range(len(alist) - 1, -1, -1):
+        result[c[alist[j]] - 1] = alist[j]
         c[alist[j]] = c[alist[j]] - 1
 
     return result
